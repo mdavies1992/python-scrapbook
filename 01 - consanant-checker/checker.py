6c8247489c51aa8a4de14257a8197f3af50722a2
Matthew Davies <m.davies.work@gmail.com>
2024-08-31 22:27:49 +0900

    initial setup for excercise

---
+++ 01 - consanant-checker/checker.py
@@ -0,0 +1,7 @@
+def consonantcheck(input):
+    print(f"{input}");
+
+print("01 - Consnant Checker")
+word = input("Please input a word or phrase to check for consonants: ")
+consonantcheck(word)
+print(f"The inputted string \"{word}\" has X consonants.")
