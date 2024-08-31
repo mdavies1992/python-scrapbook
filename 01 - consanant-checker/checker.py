76bedc042d4c4ab0fecb0e85ca250d6f0f9da034
Matthew Davies <m.davies.work@gmail.com>
2024-08-31 23:38:40 +0900

    initial try of the function, works as intended for consonants, vowels, symbols and uppercase scenarios

--- 01 - consanant-checker/checker.py
+++ 01 - consanant-checker/checker.py
@@ -1,7 +1,19 @@
 def consonantcheck(input):
-    print(f"{input}");
+    consonants = ["b", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "w", "v", "x", "y", "z"]
+    counter = 0
+    breakdown = list(input)
+    for letter in breakdown:
+        if letter.lower() in consonants:
+            counter += 1
+    return counter
+
+
 
 print("01 - Consnant Checker")
 word = input("Please input a word or phrase to check for consonants: ")
-consonantcheck(word)
-print(f"The inputted string \"{word}\" has X consonants.")
+result = consonantcheck(word)
+print(f"The inputted string \"{word}\" has {result} consonants.")
+
+# test = list("input")
+# for letter in test:
+#     print(letter)
