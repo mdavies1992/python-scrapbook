f1acfe2003b59c3c245d171507d6a647aa76db8d
Matthew Davies <m.davies.work@gmail.com>
2024-09-01 20:53:18 +0900

    started excercise but requires more time to think on, so saving this one for later

--- 01 - consanant-checker/checker.py
+++ 01 - consanant-checker/checker.py
@@ -13,7 +13,3 @@ print("01 - Consnant Checker")
 word = input("Please input a word or phrase to check for consonants: ")
 result = consonantcheck(word)
 print(f"The inputted string \"{word}\" has {result} consonants.")
-
-# test = list("input")
-# for letter in test:
-#     print(letter)
