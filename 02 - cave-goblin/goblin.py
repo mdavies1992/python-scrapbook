86fd25e5c4fc86070d56d3f792f5df4fcf7a4bb4
Matthew Davies <m.davies.work@gmail.com>
2024-09-02 01:26:58 +0900

    finish up later

--- 02 - cave-goblin/goblin.py
+++ 02 - cave-goblin/goblin.py
@@ -1,5 +1,13 @@
+complete = 0
 heroactions = ["1] - Attack", "2] - Block", "3] - Run Away!"]
 goblinactions = ["Stab", "Growl"]
+choice = ""
+herohp = 10
+goblinhp = 10
+
+def healthcheck():
+    print(f"# Your Hit Points are at {herohp} #")
+    print(f"# The Goblin's Hit Points are at {goblinhp} #")
 
 def determineheroaction():
     print("- ACTIONS AVAILABLE: -")
@@ -10,4 +18,7 @@ def determineheroaction():
 
 print("You are in a cave, facing down an evil goblin with a very pointy stick. It's time to fight!")
 print("-")
-print(determineheroaction())
+while complete == 0:
+    healthcheck()
+    choice = determineheroaction()
+    complete += 1
