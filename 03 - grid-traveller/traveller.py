1e6bd148f01bee5f9ad882da147170b1166554bd
Matthew Davies <m.davies.work@gmail.com>
2024-09-06 21:55:22 +0900

    directions method sorted

--- 03 - grid-traveller/traveller.py
+++ 03 - grid-traveller/traveller.py
@@ -30,33 +30,36 @@ def listactions(currentposition):
     elif currentposition == "c":
         print("You are at the goal!")
     elif currentposition == "d":
-        print("You can move Up, [U], Down [D], Left [L] or Right [R]")
+        print("You can move Up [U], Down [D] or Right [R]")
     elif currentposition == "e":
-        print("You can move Up, [U], Down [D], Left [L] or Right [R]")
+        print("You can move Up [U], Down [D], Left [L] or Right [R]")
     elif currentposition == "f":
-        print("You can move Up, [U], Down [D], Left [L] or Right [R]")
+        print("You can move Up [U], Down [D] or Left [L]")
     elif currentposition == "g":
-        print("You can move Up, [U], Down [D], Left [L] or Right [R]")
+        print("You can move Up [U] or Right [R]")
+        complete = True
     elif currentposition == "h":
-        print("You can move Up, [U], Down [D], Left [L] or Right [R]")
+        print("You can move Up [U], Left [L] or Right [R]")
     elif currentposition == "i":
-        print("You can move Up, [U], Down [D], Left [L] or Right [R]")
+        print("You can move Up [U] or Left [L]")
     else:
         print("I don't know how you got here, but you're off the grid now, good luck.")
         complete = True
 
-def choosemove():
-    print("hello")
+def choosemove(currentposition):
+    print(currentposition)
 
 def updategrid(choice):
-    print(choice)
+    print("test")
 
 
 print("- GAME START -")
 print("You are in the bottom-left corner of this grid. You can move one square in x and y axis', depending on your location. Find your way to the top-right of the grid.")
 printgrid()
 while complete != True:
-    listactions("a")
+    listactions(currentposition)
     if complete != True:
         choice = choosemove(currentposition)
         updategrid(choice)
+        complete = True
+print("- GAME END -")
