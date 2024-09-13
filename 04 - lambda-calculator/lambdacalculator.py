ca4c77637408f209ebf42c68b16464274ea417b0
Matthew Davies <m.davies.work@gmail.com>
2024-09-13 23:27:53 +0900

    found better, cleaner way to do the lambda calculator

--- 04 - lambda-calculator/lambdacalculator.py
+++ 04 - lambda-calculator/lambdacalculator.py
@@ -1,25 +1,40 @@
 # OBJECTIVE: Using Lambda functions, create a script that will
 # take 2 numbers and an operator from the user,
 #  then calculate and state the result.
+import operator
 
+# FIRST VERSION - Works, but is not very DRY
+# print("- START -")
+# print("Welcome to the Lambda Calculator.")
+# first_number = int(input("Please enter the first number: "))
+# second_number = int(input("Please enter the second number: "))
+# oper = input("Please enter the oper for the two numbers [ + | - | / | * ]: ")
+# if oper == "+":
+#     result = lambda a, b: a + b
+#     print(f"The total is: {result(first_number, second_number)}")
+# elif oper == "-":
+#     result = lambda a, b: a - b
+#     print(f"The total is: {result(first_number, second_number)}")
+# elif oper == "/":
+#     result = lambda a, b: a / b
+#     print(f"The total is: {result(first_number, second_number)}")
+# elif oper == "*":
+#     result = lambda a, b: a * b
+#     print(f"The total is: {result(first_number, second_number)}")
+# else:
+#     print("That is an invalid operator. Please try again.")
 
+# print("- END -")
+
+# SECOND VERSION - Using lambda, but also importing operator/using to better create shorter code
+ops = { "+": operator.add, "-": operator.sub, "/": operator.truediv, "*": operator.mul }
 print("- START -")
 print("Welcome to the Lambda Calculator.")
 first_number = int(input("Please enter the first number: "))
 second_number = int(input("Please enter the second number: "))
-operator = input("Please enter the operator for the two numbers [ + | - | / | * ]: ")
-if operator == "+":
-    result = lambda a, b: a + b
-    print(f"The total is: {result(first_number, second_number)}")
-elif operator == "-":
-    result = lambda a, b: a - b
-    print(f"The total is: {result(first_number, second_number)}")
-elif operator == "/":
-    result = lambda a, b: a / b
-    print(f"The total is: {result(first_number, second_number)}")
-elif operator == "*":
-    result = lambda a, b: a * b
-    print(f"The total is: {result(first_number, second_number)}")
+oper = input("Please enter the oper for the two numbers [ + | - | / | * ]: ")
+if oper == "+" or "-" or "/" or "*":
+    print(f"The total is: {ops[oper](first_number, second_number)}")
 else:
     print("That is an invalid operator. Please try again.")
 
